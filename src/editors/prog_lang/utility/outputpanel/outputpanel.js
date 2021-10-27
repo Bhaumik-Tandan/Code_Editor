388b627c438c557125dbefd440c608b4e5c1725b
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-10-27 07:28:55 +0530

    position adjusted

--- src/editors/prog_lang/utility/outputpanel/outputpanel.js
+++ src/editors/prog_lang/utility/outputpanel/outputpanel.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import "./outputPanel.css";
 
 import OutputConsole from './outputConsole/outputConsole';
 function OutputPanel() {
     return (
-        <div className="output">
+        <div className="output" style={{position:"absolute",top:"66%",left:"2.5%",width:"95%"}}>
             <center><h1>OUTPUT</h1></center>
             <OutputConsole/>
         </div>
